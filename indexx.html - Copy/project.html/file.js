1db9b092ca429ee3d7e7fc599126331d2658e8b1
eshanusmani123 <eshanusmani123@gmail.com>
2025-05-29 10:29:30 +0500

    Add files via upload

---
+++ indexx.html - Copy/project.html/file.js
@@ -0,0 +1,27 @@
+let name = "John";
+console.log("hi, this is", name);
+let age;
+console.log( "hi, this is", age);
+let fruits = ["apple", "banana", "mango"];
+console.log("hi ,this is",length);
+let x = 10;
+if (x > 5) {
+    console.log("Greater");
+} else {
+    console.log("Smaller");
+}
+let isLoggedIn = true;
+
+if (isLoggedIn) {
+    console.log("hi, you are logged in");
+} else {
+    console.log("hi, you are not logged in");
+}
+
+if (isLoggedIn) {
+    console.log("welcome");
+} else {
+    console.log("Please log in.");
+}
+let message = "Hello World";
+console.log("hi, this is", message)
